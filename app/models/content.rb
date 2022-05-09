daeb81952b18ce5a8ddc8990fbdfd6e2fdc30afa
ITAgiuseppe <giuseppe.archeo@yahoo.it>
2022-05-09 15:59:11 +0200

    Pulizia e ordine del codice application

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,5 @@
 class Content < ApplicationRecord
 
   
-  has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" },
-  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
+  
 end
